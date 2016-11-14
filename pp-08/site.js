e832ee9061d225cad226f7bc8d414b56c6210a02
Scott Rigg <srigg@hawk.iit.edu>
2016-11-14 01:33:47 -0600

    Took the $ variable out of the global namespace and removed jQuery control of $

--- pp-08/site.js
+++ pp-08/site.js
@@ -1,14 +1,16 @@
-$(document).ready(
+$.noConflict();
+(function($){
+  $(document).ready(
   function() {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
-
     if (typeof(window.$) !== "undefined") {
       console.log('jQuery still controls the $ variable');
     } else {
       console.log('Nice work! jQuery no longer controls the $ variable');
     }
-
   }
 );
+})
+(jQuery);
 
